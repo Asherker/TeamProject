f82acb97b889b7c644f9a95860e803ab828cc1f3
maxkuo625 <a558424069@gmail.com>
2023-07-14 10:37:00 +0800

    新增RewriteController

--- src/main/java/com/uch/vueproject/controller/RewriteController.java
+++ src/main/java/com/uch/vueproject/controller/RewriteController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import com.uch.vueproject.bean.MySQLConfigBean;
 import com.uch.vueproject.model.BaseResponse;
-import com.uch.vueproject.model.GameEntity;
+import com.uch.vueproject.model.RecallEntity;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -18,23 +18,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
 @RestController
-public class RecallController {
+public class RewriteController {
     @Autowired
     private MySQLConfigBean mysqlb;
 
-    @RequestMapping(value = "/recall", method = RequestMethod.POST,
-    consumes = MediaType.APPLICATION_JSON_VALUE, 
+    @RequestMapping(value = "/rewrite", method = RequestMethod.POST,
+    consumes = MediaType.APPLICATION_JSON_VALUE,
     produces = MediaType.APPLICATION_JSON_VALUE)
-    public BaseResponse recall(@RequestBody GameEntity data){
+    public BaseResponse rewrite(@RequestBody RecallEntity data){
     Connection conn = null;
     PreparedStatement stmt =null;
-    PreparedStatement stmt2 = null;
     try{
         Class.forName(mysqlb.getDriverClassName());
-            
-        conn = DriverManager.getConnection(mysqlb.getUrl() + mysqlb.getData()+ "?user=" + mysqlb.getUsername() + "&password=" + mysqlb.getPassword());
 
-        stmt = conn.prepareStatement("INSERT INSERT INTO trackinghistory VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        conn = DriverManager.getConnection(mysqlb.getUrl() + mysqlb.getData()+ "?user=" + mysqlb.getUsername() + "&password=" + mysqlb.getKuo());
+
+        stmt = conn.prepareStatement("UPDATE trackinghistory SET name=?, category=?, developer=?, platform=?, price=?, quantity=?, inchange=?, outchange=? WHERE id=?");
         stmt.setString(1, data.getId());
         stmt.setString(2, data.getName());
         stmt.setString(3, data.getPlatform());
@@ -44,15 +43,17 @@ public class RecallController {
         stmt.setInt(7, data.getQuantity());
         stmt.setDate(8, data.getInchange());
         stmt.setDate(9, data.getOutchange());
+        stmt.setDate(10, data.getFinalupdate());
 
         stmt.executeUpdate();
 
         return new BaseResponse(0, "已新增舊有資料至歷史資料表");
-            
-        }catch(SQLException e) {
-            return new BaseResponse(e.getErrorCode(), e.getMessage());
-        }catch(ClassNotFoundException e) {
-            return new BaseResponse(5,"歷史紀錄新增失敗");
-        }
+    }catch(SQLException e) {
+        return new BaseResponse(e.getErrorCode(), e.getMessage());
+    }catch(ClassNotFoundException e) {
+        return new BaseResponse(5,"歷史紀錄新增失敗");
     }
+    }
+        
+    
 }
